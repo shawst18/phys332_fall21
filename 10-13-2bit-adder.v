8c8196143a6a73496eac5d8c30054676626ab4c4
Stephen Shaw <stephen.shaw120@gmail.com>
2021-10-13 12:19:37 -0500

    Update 10-13-2bit-adder.v
    Defined C_out and sum in module fulladder, fixed switch input and LED definitions so 4 inputs and 3 outputs, etc.

--- 10-13-2bit-adder.v
+++ 10-13-2bit-adder.v
@@ -4,25 +4,25 @@
 // code is initially broken.  See attached photos for intended design
 
 module example1(
-    input [1:0] swt,
-    output [1:0] led
+    input [3:0] swt,
+    output [2:0] led
     );
     wire a1, a0, b1, b0;
-    wire s1, s0, cout;
+    wire s1, s0, cout, c_out1;
     
     // global inputs
     assign a0 = swt[0];
-    assign a1 = swt[0];
-    assign b0 = swt[0];
-    assign b1 = swt[0];
+    assign a1 = swt[1];
+    assign b0 = swt[2];
+    assign b1 = swt[3];
     
     // global outputs
     assign led[0] = s0;
     assign led[1] = s1;
     assign led[2] = cout;
     
-    fulladder add0(a,b,c_in, c_out, s1);
-    fulladder add1(a,b,c_in, cout, s1);
+    fulladder a0(a0,b0,0, c_out1, s0);
+    fulladder a1(a1,b1,c_out1, cout, s1);
         
 endmodule 
 
@@ -30,9 +30,9 @@ module fulladder(
     input a,
     input b,
     input c_in,
-    output c_out
+    output c_out,
     output sum
     );
-    assign c_out = ;
-    assign sum = ;
+    assign c_out = (a&b)|(c_in&b)|(c_in&a);
+    assign sum = (~a&~b&c)|(~a&b&~c)|(a&b&c)|(a&~b&~c);
 endmodule
