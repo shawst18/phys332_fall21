0108a3a9e994785aa5ecb7834734c93705e91eeb
Stephen Shaw <stephen.shaw120@gmail.com>
2021-10-13 12:41:17 -0500

    Update 10-13-2bit-adder.v
    Fixed swt to SW for lab constraint file

--- 10-13-2bit-adder.v
+++ 10-13-2bit-adder.v
@@ -1,40 +1,40 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// this is a 2bit + 2bit adder for Nexys4 DDR board 
+// Module Name: lab6
+//////////////////////////////////////////////////////////////////////////////////
+
 // code is initially broken.  See attached photos for intended design
 
-module example1(
-    input [1:0] swt,
-    
-    //Need to declare three LED's to match global outputs 
-    output [2:0] led
+module addedx2bit(
+    input [3:0] SW,
+    output [2:0] LED
     );
     wire a1, a0, b1, b0;
-    wire s1, s0, cout, c_out;
-    
+    wire s1, s0, cout, c_out1;
+
     // global inputs
-    assign a0 = swt[0];
-    assign a1 = swt[0];
-    assign b0 = swt[0];
-    assign b1 = swt[0];
-    
+    assign a0 = SW[0];
+    assign a1 = SW[1];
+    assign b0 = SW[2];
+    assign b1 = SW[3];
+
     // global outputs
-    assign led[0] = s0;
-    assign led[1] = s1;
-    assign led[2] = cout;
-    
-    fulladder add0(a0, b0, 0, c_out, s0);
-    fulladder add1(a1, b1, c_out, cout, s1);
-        
+    assign LED[0] = s0;
+    assign LED[1] = s1;
+    assign LED[2] = cout;
+
+    fulladder a0(a0,b0,0, c_out1, s0);
+    fulladder a1(a1,b1,c_out1, cout, s1);
+
 endmodule 
 
 module fulladder(
     input a,
     input b,
     input c_in,
-    output c_out
+    output c_out,
     output sum
     );
-    assign c_out = (a & b) | (a & c_in) | (b & c_in);
-    assign sum = (~a & ~b & c) | (~a & b & ~c) | (a & b & c) | (a & ~b & ~c);
+    assign c_out = (a&b)|(c_in&b)|(c_in&a);
+    assign sum = (~a&~b&c_in)|(~a&b&~c_in)|(a&b&c_in)|(a&~b&~c_in);
 endmodule
